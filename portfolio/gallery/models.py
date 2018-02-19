118a2b8027bd7410d537151e5d456cda4300c89f
Dylan Peter Hayward <dylanpeterhayward@gmail.com>
2018-02-19 21:31:55 +0100

    Add custom client + url to each image.

--- portfolio/gallery/models.py
+++ portfolio/gallery/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailadmin.edit_handlers import InlinePanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailimages.models import Image, AbstractImage, AbstractRendition
 
 from modelcluster.fields import ParentalKey
 
@@ -12,7 +13,7 @@ class GalleryPage(Page):
     """ Displays a featured image to represent a set of gallery images. """
     featured_image = models.ForeignKey(
         'wagtailimages.Image', on_delete=models.PROTECT, related_name='+',
-        verbose_name=_("featured image"), null=True
+        verbose_name=_("featured image"), null=True,
     )
 
     content_panels = Page.content_panels + [
@@ -21,14 +22,50 @@ class GalleryPage(Page):
     ]
 
 
+class Img(AbstractImage):
+    client_name = models.CharField(
+        verbose_name=_("client name"),
+        max_length=100,
+        blank=True, null=True,
+    )
+    client_url = models.URLField(
+        verbose_name=_("client url"),
+        blank=True, null=True,
+    )
+
+    admin_form_fields = (
+        'title',
+        'file',
+        'client_name',
+        'client_url',
+        'focal_point_x',
+        'focal_point_y',
+        'focal_point_width',
+        'focal_point_height',
+    )
+
+
+class CustomRendition(AbstractRendition):
+    image = models.ForeignKey(Img, related_name='renditions')
+
+    class Meta:
+        unique_together = (
+            ('image', 'filter_spec', 'focal_point_key'),
+        )
+
+
 class GalleryImage(Orderable):
     """ Individual image that can be sorted into a GalleryPage."""
     page = ParentalKey(
-        GalleryPage, on_delete=models.PROTECT, related_name='gallery_images'
+        GalleryPage,
+        on_delete=models.PROTECT,
+        related_name='gallery_images',
     )
     image = models.ForeignKey(
-        'wagtailimages.Image', on_delete=models.PROTECT, related_name='+',
-        verbose_name=_("image")
+        'gallery.Img',
+        on_delete=models.PROTECT,
+        related_name='+',
+        verbose_name=_("image"),
     )
 
     panels = [
--- portfolio/portfolio/settings/base.py
+++ portfolio/portfolio/settings/base.py
@@ -140,3 +140,4 @@ WAGTAIL_SITE_NAME = "portfolio"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'https://anyurotyakmaz.com'
+WAGTAILIMAGES_IMAGE_MODEL = 'gallery.Img'
