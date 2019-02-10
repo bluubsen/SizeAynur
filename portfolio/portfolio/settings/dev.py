0864947c052e2a00ae63950af4925aed9f91b87b
Dylan Hayward <dylanpeterhayward@gmail.com>
2019-02-10 17:46:53 +0100

    Commit

--- portfolio/portfolio/settings/dev.py
+++ portfolio/portfolio/settings/dev.py
@@ -7,7 +7,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "aynurotakmaz.com",
+        "NAME": "aynurotakmaz",
         "USER": "admin",
         "PASSWORD": "admin",
         "HOST": "127.0.0.1",
