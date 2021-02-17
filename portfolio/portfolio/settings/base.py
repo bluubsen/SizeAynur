534326f526aab5527db147617d9ada9dd2486736
Dylan Hayward <dylanpeterhayward@gmail.com>
2021-02-17 17:19:18 +0000

    Update url in base settings.

--- portfolio/portfolio/settings/base.py
+++ portfolio/portfolio/settings/base.py
@@ -125,4 +125,4 @@ WAGTAIL_SITE_NAME = "portfolio"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'https://anyurotyakmaz.com'
+BASE_URL = 'https://aynurotyakmaz.com/'
